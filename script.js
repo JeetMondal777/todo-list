a6bffe398a2d1eb33d0ef6c547d7331559e91e62
Jeet Mondal <jeetmn0226@gmail.com>
2024-07-28 11:54:06 +0530

    making a new feature which transform the todo list into a task reminder

--- script.js
+++ script.js
@@ -1,72 +1,115 @@
-
 let todoInput = document.getElementById('todo-input');
 let addBtn = document.getElementById('add-btn');
 let time = document.querySelector("#timeInput")
+let date = document.querySelector("#dateInput")
 let text = document.querySelector("#text");
 
-addBtn.addEventListener('click', function(){
-    if(todoInput.value ==""){
-        alert("Please Enter A Task With Time");
-    }else{
-    let newElem = document.createElement("ul");
-    newElem.innerHTML = `<div id="inputText">${todoInput.value}</div> at 
-    <div id="declaredTime">${time.value}</div>
-    <div id="trash"><i class="ri-delete-bin-6-fill"></i></div>`;
-    text.appendChild(newElem)
-    
-   newElem.querySelector("i").addEventListener("click", removeElem);
-   function removeElem(){
-        newElem.remove();
+let currentTime;
+let notificationDelay = 120000;
+
+if("Notification" in window)
+    {Notification.requestPermission().then(perm => {
+    if(perm !== "granted"){
+        alert("please turn on notification")
+        location.reload()
     }
+    if(perm === "granted"){
+        alert(` We Are Ready To Send You Notification About Your Tasks`)
+    }}
+)}
+
+let allTimes = [];
+let allIntervals = []; // Store interval IDs of all tasks
+
+function scheduleReminder(){
     
-    let textInput = newElem.querySelector("#inputText")
-    let timeDeclared = newElem.querySelector("#declaredTime")
+    let dateTime = new Date(`${date.value}T${time.value}`);
+    let scheduledTime = dateTime;
+    currentTime = new Date()
+    let timeDifference = scheduledTime - currentTime
+    let newElem
     let check = 0
 
-    textInput.addEventListener("click",function(){
-        if(check==0){
-            console.log(textInput.value)
-        textInput.style.textDecoration = "line-through"
-        textInput.style.opacity = "0.3"
-        timeDeclared.style.textDecoration = "line-through"
-        timeDeclared.style.opacity = "0.3"
-        check =1
-    }else{
-        textInput.style.textDecoration = "none"
-        textInput.style.opacity = "1"
-        timeDeclared.style.textDecoration = "none"
-        timeDeclared.style.opacity = "1"
-        check =0
-    }
-})
+    if(timeDifference>0){
+    newElem = document.createElement("ul");
+    newElem.innerHTML = `<div id="inputText">${todoInput.value}</div> at
+    <div id="declaredTime"> ${time.value}</div>on
+    <div id="declaredDate"> ${date.value}</div>
+    <div id="trash"><i class="ri-delete-bin-6-fill"></i></div>`;
+    let delIcon = newElem.querySelector("#trash"); // Store the reference to delIcon
+    text.appendChild(newElem)
+    
+    function addReminder(todoInput, dateTime) {
+        let timeoutId = setTimeout(() => {
+          new Notification("Your Task", {
+            body: `${todoInput} at ${dateTime} is pending`,
+            requiredInteraction: true,
+          });
+          newElem.style.color = "red";
+          let intervalId = setInterval(() => {
+            currentTime = new Date(); // Update the currentTime
+            if (currentTime >= scheduledTime) {
+              new Notification(`pending task`, {
+                body: `your ${todoInput} is pending`,
+                requiredInteraction: true,
+              })
+            } else {
+              clearInterval(intervalId); // Clear the interval if the scheduled time has not passed
+            }
+          },notificationDelay)
+          allIntervals.push({intervalId, newElem}); // Store interval ID and corresponding task element
+        }, timeDifference);
 
-    timeDeclared.addEventListener("click",function(){
-        if(check==0){
-        timeDeclared.style.textDecoration = "line-through"
-        timeDeclared.style.opacity = "0.3"
-        textInput.style.textDecoration = "line-through"
-        textInput.style.opacity = "0.3"
-        check =1
-    }else{
-        timeDeclared.style.textDecoration = "none"
-        timeDeclared.style.opacity = "1"
-        textInput.style.textDecoration = "none"
-        textInput.style.opacity = "1"
-        check =0
-    }
-})
+        newElem.addEventListener("click",function(){
+          if(check===0){
+              newElem.style.textDecoration = "line-through"
+              newElem.style.opacity = "0.3"
+              allIntervals.forEach((item) => {
+                if (item.newElem === newElem) {
+                  clearInterval(item.intervalId);
+                }
+              });
+              check =1
+          }else{
+              newElem.style.textDecoration = "none"
+              newElem.style.opacity = "1"
+              check =0
+          }
+      })
 
-    Notification.requestPermission().then(perm => {
-        if(perm === "granted"){
-            new Notification(` at ${time.value} ${todoInput.value} pending`)
-            todoInput.value = "";
-        }
-    })
+    
+    delIcon.addEventListener("click", function(){
+      if(check===0){
+        allIntervals.forEach((item, index) => {
+          if (item.newElem === newElem) {
+            clearInterval(item.intervalId);
+            allIntervals.splice(index, 1);
+          }
+        });
+        newElem.remove();
+      }
+    });
+
+        
+        allTimes.push(timeoutId);
+      }
+    addReminder(todoInput.value, dateTime);
+
+    }if(timeDifference<=0){
+        alert("Your scheduled Time Is In The Past")
+    }
+
+}
     
 
-    }})
+addBtn.addEventListener('click', function(){
+    if(todoInput.value ==="" || time.value === ""){
+        alert("Please Enter A Task With Time");
+    }else{
 
+    scheduleReminder();
+    
 
-
-
-
+} 
+    
+})
